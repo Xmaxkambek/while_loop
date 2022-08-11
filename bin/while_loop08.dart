0b92ffa010618e4670a4a0ccfc8546aeb3f54b52
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-11 10:39:38 +0500

    08

--- bin/while_loop08.dart
+++ bin/while_loop08.dart
@@ -4,4 +4,19 @@
   
   
 */
-void main() {}
+
+ String func(int a){
+int s = 0;
+String d = '';
+while(s<=10){
+  if(s % 2 == 1){
+    d += s.toString();
+  }
+  s += 1;
+
+}
+return d;
+ }
+void main() {
+  print(func(10));
+}
